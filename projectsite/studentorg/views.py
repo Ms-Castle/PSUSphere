7a55dc70c5eb6516c0cf5960d931a7597c17f84d
[Sand-Kim] <[sandycastillo651@gmail.com]>
2025-04-02 22:08:03 +0800

    Search Feature

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -6,6 +6,10 @@ from studentorg.models import Organization, OrgMember, Student, College, Program
 from studentorg.forms import OrganizationForm
 from django.urls import reverse_lazy
 
+from typing import Any
+from django.db.models.query import QuerySet
+from django.db.models import Q
+
 # Home Page View
 class HomePageView(ListView):
     model = Organization
@@ -18,6 +22,14 @@ class OrganizationList(ListView):
     context_object_name = 'organization'
     template_name = 'org_list.html'
     paginate_by = 5
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super(OrganizationList, self).get_queryset(*args, **kwargs)
+        if self.request.GET.get("q") != None:
+            query = self.request.GET.get('q')
+            qs = qs.filter(Q(name__icontains=query) |
+                            Q(description__icontains=query))
+            return qs
 
 class OrganizationCreateView(CreateView):
     model = Organization
